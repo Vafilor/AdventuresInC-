135a591efc6ee6f6b558130a79075b0580b11ce5
Vafilor <vafilor@gmail.com>
2016-05-06 16:19:29 -0700

    Added matrix multiplication.

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -4,31 +4,29 @@
 
 class Matrix
 {
-	private:
-		double** entries;
-		unsigned int rows;
-		unsigned int columns;
-		
-		void	scaleInPlace(double scalar);
-		//TODO static method that determines if matrix sizes match
-		
-	public:
-		Matrix(unsigned int rows, unsigned int columns);
-		Matrix(Matrix* matrix);
-		~Matrix();
-		
-		//TODO make a public static method to generate identity matrix for some size and fill with random
-		//values - one where random values are provided by some generator too.
-		unsigned int getRows();
-		unsigned int getColumns();
-		double getEntry(int row, int column); //TODO make double array accessor?
-		void setEntry(int row, int column, double value);
-		//Make operator versions of these. Want A + B, A - B
-		Matrix* add(Matrix* that); //TODO - is this passed by value?
-		Matrix* subtract(Matrix* that);
-		Matrix* scale(double scalar);
-		
- 
-		//Matrix multiply(Matrix that): //Passed by ref or val? Performance issue?
-		std::string toString();
+    private:
+        double** entries;
+        unsigned int rows;
+        unsigned int columns;
+        
+        void    scaleInPlace(double scalar);
+        //TODO static method that determines if matrix sizes match
+        
+    public:
+        Matrix(unsigned int rows, unsigned int columns);
+        Matrix(Matrix* matrix);
+        ~Matrix();
+        
+        //TODO make a public static method to generate identity matrix for some size and fill with random
+        //values - one where random values are provided by some generator too.
+        unsigned int getRows();
+        unsigned int getColumns();
+        double getEntry(int row, int column); //TODO make double array accessor?
+        void setEntry(int row, int column, double value);
+        //Make operator versions of these. Want A + B, A - B
+        Matrix* add(Matrix* that);
+        Matrix* subtract(Matrix* that);
+        Matrix* scale(double scalar);
+        Matrix* multiply(Matrix* that);
+        std::string toString();
 };
