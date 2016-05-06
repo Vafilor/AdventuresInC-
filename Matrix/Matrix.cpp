26ca2301e83d1bb517a3d9d2bb0e8b828ca735bb
Vafilor <vafilor@gmail.com>
2016-05-06 16:50:53 -0700

    Fixed an issue where allocated/deallocated incorrect amount of memory in Matrix class.

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -1,133 +1,163 @@
 #include "Matrix.h"
 #include <iostream> //For debugging
+#include <string>
 using namespace std; //For debugging
 
 Matrix::Matrix(unsigned int rows, unsigned int columns)
 {
-	double** data = new double*[rows];
-	
-	for(int i = 0; i < columns; i++) 
-	{
-		data[i] = new double[columns]; //TODO can you auto initialize dynamic arrays?
-		
-		for(int j = 0; j < columns; j++) 
-		{
-			data[i][j] = 0.0;
-		}
-	}
-	
-	this->entries = data;
-	this->rows = rows;
-	this->columns = columns;
+    double** data = new double*[rows];
+    
+    for(int i = 0; i < rows; i++) 
+    {
+        data[i] = new double[columns]; //TODO can you auto initialize dynamic arrays?
+        
+        for(int j = 0; j < columns; j++) 
+        {
+            data[i][j] = 0.0;
+        }
+    }
+    
+    this->entries = data;
+    this->rows = rows;
+    this->columns = columns;
 }
 
 Matrix::Matrix(Matrix* matrix)
 {
-	double** data = new double*[matrix->getRows()];
-	
-	for(int i = 0; i < matrix->getRows(); i++)
-	{
-		data[i] = new double[matrix->getColumns()];
-		
-		for(int j = 0; j < matrix->getColumns(); j++)
-		{
-			data[i][j] = matrix->getEntry(i,j);
-		}
-	}
-	
-	this->entries = data;
-	this->rows = matrix->getRows();
-	this->columns = matrix->getColumns();
+    double** data = new double*[matrix->getRows()];
+    
+    for(int i = 0; i < matrix->getRows(); i++)
+    {
+        data[i] = new double[matrix->getColumns()];
+        
+        for(int j = 0; j < matrix->getColumns(); j++)
+        {
+            data[i][j] = matrix->getEntry(i,j);
+        }
+    }
+    
+    this->entries = data;
+    this->rows = matrix->getRows();
+    this->columns = matrix->getColumns();
 }
 
-//TODO make sure all data is dealloced
 Matrix::~Matrix()
 {
-	for(int i = 0; i < this->rows; i++) 
-	{
-		delete[] this->entries[i];
-	}
+    for(int i = 0; i < this->rows; i++) 
+    {	
+		cout << i << " ";
+        delete[] this->entries[i];
+    }
+	delete[] this->entries;
 }
 
 unsigned int Matrix::getRows()
 {
-	return this->rows;
+    return this->rows;
 }
 
 unsigned int Matrix::getColumns()
 {
-	return this->columns;
+    return this->columns;
 }
 
 double Matrix::getEntry(int row, int column)
 {
-	return this->entries[row][column];
+    return this->entries[row][column];
 }
 
 
 void Matrix::setEntry(int row, int column, double value)
 {
-	this->entries[row][column] = value;
+    this->entries[row][column] = value;
 }
 
 Matrix* Matrix::add(Matrix* that)
 {
-	//TODO - if incompatible sizes, throw exception
+    //TODO - if incompatible sizes, throw exception
 
-	Matrix* copy = new Matrix(this);
-	
-	for(int i = 0; i < this->getRows(); i++)
-	{
-		for(int j = 0; j < this->getColumns(); j++)
-		{
-			copy->setEntry(i, j, this->getEntry(i, j) + that->getEntry(i,j) );
-		}
-	}	
-	
-	return copy;	
+    Matrix* copy = new Matrix(this);
+    
+    for(int i = 0; i < this->getRows(); i++)
+    {
+        for(int j = 0; j < this->getColumns(); j++)
+        {
+            copy->setEntry(i, j, this->getEntry(i, j) + that->getEntry(i,j) );
+        }
+    }    
+    
+    return copy;    
 }
 
 //TODO make sure you mark what's const and what's not
 Matrix* Matrix::subtract(Matrix* that)
 {
-	//TODO - if incompatible sizes, throw exception
-	Matrix* temp = that->scale(-1.0);
-	Matrix* result = this->add(temp);
-	
-	delete temp;
-	
-	return result;
-	
-	
-	//Is there performance penalty vs below?
-// 	Matrix* copy = new Matrix(this);
-// 	
-// 	for(int i = 0; i < this->getRows(); i++)
-// 	{
-// 		for(int j = 0; j < this->getColumns(); j++)
-// 		{
-// 			copy->setEntry(i, j, this->getEntry(i, j) - that->getEntry(i,j) );
-// 		}
-// 	}	
-// 	
-// 	return copy;	
+    //TODO - if incompatible sizes, throw exception
+    Matrix* temp = that->scale(-1.0);
+    Matrix* result = this->add(temp);
+    
+    delete temp;
+    
+    return result;
+    
+    
+    //Is there performance penalty vs below?
+//     Matrix* copy = new Matrix(this);
+//     
+//     for(int i = 0; i < this->getRows(); i++)
+//     {
+//         for(int j = 0; j < this->getColumns(); j++)
+//         {
+//             copy->setEntry(i, j, this->getEntry(i, j) - that->getEntry(i,j) );
+//         }
+//     }    
+//     
+//     return copy;    
 }
 
 Matrix* Matrix::scale(double scalar)
 {
-	//TODO - if incompatible sizes, throw exception
+    //TODO - if incompatible sizes, throw exception
 
-	Matrix* copy = new Matrix(this);
-	
-	for(int i = 0; i < this->getRows(); i++)
-	{
-		for(int j = 0; j < this->getColumns(); j++)
-		{
-			copy->setEntry(i, j,  this->getEntry(i, j) * scalar );
-		}
-	}	
-	
-	return copy;
+    Matrix* copy = new Matrix(this);
+    
+    for(int i = 0; i < this->getRows(); i++)
+    {
+        for(int j = 0; j < this->getColumns(); j++)
+        {
+            copy->setEntry(i, j,  this->getEntry(i, j) * scalar );
+        }
+    }    
+    
+    return copy;
+}
+
+Matrix* Matrix::multiply(Matrix* that)
+{
+    //TODO dimension requirements (A by B) x (B by C)
+    int newRows = this->getRows();
+    int newColumns = that->getColumns();
+
+    Matrix* product = new Matrix( newRows, newColumns );
+    
+    double newValue = 0.0;
+    
+    for(int i = 0; i < newRows; i++) 
+    {
+        for(int j = 0; j < newColumns; j++)
+        {
+            for(int k = 0; k < this->getColumns(); k++)
+            {
+                newValue += this->getEntry(i, k) * that->getEntry(k, j);
+            }
+            
+            product->setEntry(i, j, newValue);
+            
+            newValue = 0.0;
+        }    
+    }
+    
+    return product;
 }
 
 std::string Matrix::toString()
@@ -150,11 +180,11 @@ std::string Matrix::toString()
 //Private Methods
 void Matrix::scaleInPlace(double scalar)
 {
-	for(int i = 0; i < this->getRows(); i++)
-	{
-		for(int j = 0; j < this->getColumns(); j++)
-		{
-			this->setEntry(i, j,  this->getEntry(i, j) * scalar );
-		}
-	}		
+    for(int i = 0; i < this->getRows(); i++)
+    {
+        for(int j = 0; j < this->getColumns(); j++)
+        {
+            this->setEntry(i, j,  this->getEntry(i, j) * scalar );
+        }
+    }        
 }
--- Matrix/tester.cpp
+++ Matrix/tester.cpp
@@ -10,6 +10,7 @@ void printMatrix(Matrix* matrix);
 int main()
 {
 	Matrix* matrix = new Matrix(3,4);
+	Matrix* matrix2 = new Matrix(4,5);
 	
 	
 	for(int i = 0; i < matrix->getRows(); i++)
@@ -20,19 +21,24 @@ int main()
 		}	
 	}
 
+	for(int i = 0; i < matrix2->getRows(); i++)
+	{
+		for(int j = 0; j < matrix2->getColumns(); j++)
+		{
+			matrix2->setEntry(i,j, i+j);
+		}	
+	}
+
 	cout << matrix->toString() << endl;
-
-	Matrix* temp = matrix->scale(10);
-
-	cout << temp->toString() << endl;
-	
-	Matrix* sum = matrix->subtract(temp);
-	
-	cout << sum->toString() << endl;
-
+	cout << matrix2->toString() << endl;
+	cout << endl;
+	Matrix* result = matrix->multiply(matrix2);
+	cout << result->toString() << endl;
 	cout << endl;
 
-	//delete matrix;
+	delete matrix;
+	delete matrix2;
+	delete result;
 
 	return 0;
 }
