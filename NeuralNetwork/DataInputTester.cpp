ff8f8d5d0d3ac62f00e6b63fd1ca34583f21b774
Vafilor <vafilor@gmail.com>
2016-07-25 16:56:42 -0700

    Updates to DataInputTester  •Updated header includes as they have been changed from .hpp to .h  •Added back function call to start SGD on Neural Network

--- NeuralNetwork/DataInputTester.cpp
+++ NeuralNetwork/DataInputTester.cpp
@@ -6,13 +6,12 @@
 #include <stdexcept>
 #include <string>
 
-#include "Matrix.hpp"
-#include "NeuralNetwork.hpp"
-#include "HandWriting.hpp"
-#include "Timer.hpp"
+#include "Matrix.h"
+#include "NeuralNetwork.h"
+#include "HandWriting.h"
+#include "Timer.h"
 
 using namespace std;
-
 
 
 //TODO - rename this to ImageData tester or similar - this is a specific use of Neural Network. Classes for this purpose are defined in a header 
@@ -25,6 +24,7 @@ int main()
 	string labelTestingPath = "";
 		
 	Timer timer;
+	timer.setVerbose(true, &cout);
 	
 	timer.mark("Load Data");
 		
@@ -33,22 +33,20 @@ int main()
 	
 	timer.mark();
 	
-	
 	vector<int> neurons;
 	neurons.push_back(28*28);
 	neurons.push_back(10);
 	neurons.push_back(10);
 	
-	NeuralNetwork network(neurons);
-	
+	NeuralNetwork network(neurons);	
 	
 	timer.mark("Net Training");
 	
-// 	network.SGD(trainingData, 1, 10, 1.0, testData );		
+	network.SGD(trainingData, 30, 10, 3.0, testingData );		
 
 	timer.mark();
-	
-	cout << timer << endl;
+
+	cout << "Program Done" << endl;	
 	
 	return 0;
 }
