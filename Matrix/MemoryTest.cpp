8808cd56685a5dcec19b82738a3cddaadcb84cb0
Vafilor <vafilor@gmail.com>
2016-08-05 12:51:20 -0700

    Matrix MemoryTester Updates  •Added operators, getters, setters

--- Matrix/MemoryTest.cpp
+++ Matrix/MemoryTest.cpp
@@ -5,7 +5,6 @@
 #include <vector>
 
 using namespace std;
-//TODO - test constructors, and all operations and methods. True, some shouldn't change memory (eg. getter/setter) but we have no knowledge of internal implementation. Only the interface and we want to make sure that works.
 
 void constructors();
 void operatorAddition();
@@ -13,16 +12,18 @@ void operatorMultiplication();
 void operatorsInto();
 void operatorsScalar();
 void operatorsFunctions();
-
+void getters();
+void setters();
 
 int main()
 {
 	 constructors();
 	 operatorAddition();
 	 operatorMultiplication();
-	 operatorsInto();
 	 operatorsScalar();
 	 operatorsFunctions();
+	 getters();
+	 setters();	 
 }
 
 void constructors()
@@ -55,16 +56,16 @@ void constructors()
 	
 	Matrix blank;
 	
-	//Move Assignment
+ 	//Move Assignment
 	blank = Matrix(3,3);
 	
-	//Copy Constructor
+ 	//Copy Constructor
 	Matrix rectangleCopy = rectangle;
 	
-	//Operator assignment
+ 	//Operator assignment
 	rectangleCopy = row;
 	
-	//Setting all to 0 matrix
+ 	//Setting all to 0 matrix
 	single = zero;
 	row = Matrix();
 	column = Matrix(0,0);
@@ -75,20 +76,195 @@ void constructors()
 
 void operatorAddition()
 {
+	Matrix zero;
+	Matrix single(1,1);
+	Matrix row(1,3);
+	Matrix column(3,1);
+	Matrix square(2,2);
+	Matrix rectangle(3,4);
+	Matrix rectangle2(4,3);
+	
+	zero + zero;
+	zero += zero;
+	
+	single + single;
+	single += single;
+	
+	row + row;
+	row += row;
+	
+	column + column;
+	column += column;
+	
+	square + square;
+	square += square;
+	
+	rectangle + rectangle;
+	rectangle += rectangle;
+	
+	rectangle2 + rectangle2;
+	rectangle2 += rectangle2;
 }
 
 void operatorMultiplication()
 {
-}
-
-void operatorsInto()
-{
+	Matrix zero;
+	Matrix single(1,1);
+	Matrix row(1,3);
+	Matrix column(3,1);
+	Matrix square(2,2);
+	Matrix rectangle(3,4);
+	Matrix rectangle2(4,3);
+	
+	zero * zero;
+	single * single;
+	
+	row * column;
+	column * row;
+	
+	square * square;
+	
+	rectangle * rectangle2;
+	rectangle2 * rectangle;
 }
 
 void operatorsScalar()
 {
+	Matrix zero;
+	Matrix single(1,1);
+	Matrix row(1,3);
+	Matrix column(3,1);
+	Matrix square(2,2);
+	Matrix rectangle(3,4);
+	Matrix rectangle2(4,3);
+	
+	vector<Matrix> matrices;
+	matrices.reserve(7);
+	
+	//Move constructor
+	matrices.push_back(zero);
+	matrices.push_back(single);
+	matrices.push_back(row);
+	matrices.push_back(column);
+	matrices.push_back(square);
+	matrices.push_back(rectangle);
+	matrices.push_back(rectangle2);
+	
+	for(Matrix& matrix : matrices) 
+	{
+		matrix *= 1.0;
+		matrix *= 2.2;
+		matrix /= 1.0;
+		matrix /= 5.3;
+	}
+	
+	matrices.clear();
 }
 
 void operatorsFunctions()
 {
+	Matrix zero;
+	Matrix single(1,1);
+	Matrix row(1,3);
+	Matrix column(3,1);
+	Matrix square(2,2);
+	Matrix rectangle(3,4);
+	Matrix rectangle2(4,3);
+	
+	vector<Matrix> matrices;
+	matrices.reserve(7);
+	
+	//Move constructor
+	matrices.push_back(zero);
+	matrices.push_back(single);
+	matrices.push_back(row);
+	matrices.push_back(column);
+	matrices.push_back(square);
+	matrices.push_back(rectangle);
+	matrices.push_back(rectangle2);
+	
+	for(Matrix& matrix : matrices) 
+	{
+		matrix.applyFunction( [](double entry){
+			return entry * entry;
+		});
+		
+		matrix.applyFunctionInto( [](double entry){
+			return entry * entry;
+		});
+	}
+	
+	matrices.clear();
+}
+
+void getters()
+{
+	Matrix zero;
+	Matrix single(1,1);
+	Matrix row(1,3);
+	Matrix column(3,1);
+	Matrix square(2,2);
+	Matrix rectangle(3,4);
+	Matrix rectangle2(4,3);
+	
+	vector<Matrix> matrices;
+	matrices.reserve(7);
+	
+	//Move constructor
+	matrices.push_back(zero);
+	matrices.push_back(single);
+	matrices.push_back(row);
+	matrices.push_back(column);
+	matrices.push_back(square);
+	matrices.push_back(rectangle);
+	matrices.push_back(rectangle2);
+	
+	for(Matrix& matrix : matrices) 
+	{
+		for(int row = 0; row < matrix.getRows(); row++)
+		{
+			for(int column = 0; column < matrix.getColumns(); column++)
+			{
+				double result = matrix(row, column);
+			}
+		}
+	}
+	
+	matrices.clear();
+}
+
+void setters()
+{
+	Matrix zero;
+	Matrix single(1,1);
+	Matrix row(1,3);
+	Matrix column(3,1);
+	Matrix square(2,2);
+	Matrix rectangle(3,4);
+	Matrix rectangle2(4,3);
+	
+	vector<Matrix> matrices;
+	matrices.reserve(7);
+	
+	//Move constructor
+	matrices.push_back(zero);
+	matrices.push_back(single);
+	matrices.push_back(row);
+	matrices.push_back(column);
+	matrices.push_back(square);
+	matrices.push_back(rectangle);
+	matrices.push_back(rectangle2);
+	
+	for(Matrix& matrix : matrices) 
+	{
+		for(int row = 0; row < matrix.getRows(); row++)
+		{
+			for(int column = 0; column < matrix.getColumns(); column++)
+			{
+				matrix(row, column) = 0.245;
+			}
+		}
+	}
+	
+	matrices.clear();
 }
